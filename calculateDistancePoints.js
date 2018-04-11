f789dec51a8812085cc23b4f743d2b4943ff1b06
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-11 23:45:26 +0200

    Added calculateDistancePoints function

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,14 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-  ...
+  const difference = distance - kPoint;
+
+  switch (hillSize) {
+    case "normal":
+      return 60 + difference * 2;
+    case "large":
+      return 60 + difference * 1.8;
+    case "sky flying":
+      return 120 + difference * 1.2;
+  }
 };
 
 module.exports = calculateDistancePoints;
