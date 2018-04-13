81b7430cc8780de9c297e36e4d3ee583ee311d94
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-13 23:53:21 +0200

    Add function calculateStylePoints

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,15 @@
-const calculateStylePoints = (styleNotes) => {
-  ...
+const calculateStylePoints = styleNotes => {
+  const minNotes = Math.min(...styleNotes);
+  const maxNotes = Math.max(...styleNotes);
+
+  const min = styleNotes.filter(x => x == minNotes).length;
+  const max = styleNotes.filter(x => x == maxNotes).length;
+
+  const total = styleNotes.filter(x => {
+    return x !== minNotes && x !== maxNotes;
+  });
+
+  total.reduce((a, b) => a + b);
 };
 
 module.exports = calculateStylePoints;
