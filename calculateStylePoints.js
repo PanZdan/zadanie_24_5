3249991983212b08a158739eea98daa87cef9e8d
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-16 00:56:49 +0200

    Changed function

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,15 +1,18 @@
 const calculateStylePoints = styleNotes => {
+  
   const minNotes = Math.min(...styleNotes);
   const maxNotes = Math.max(...styleNotes);
 
-  const min = styleNotes.filter(x => x == minNotes).length;
-  const max = styleNotes.filter(x => x == maxNotes).length;
+  const minIndex = styleNotes.filter(x => x == minNotes).length;
+  const maxIndex = styleNotes.filter(x => x == maxNotes).length;
 
-  const total = styleNotes.filter(x => {
+  const total = styleNotes.filter((x) => {
     return x !== minNotes && x !== maxNotes;
   });
 
-  total.reduce((a, b) => a + b);
+  const points = total.reduce((a, b) => a + b, 0);
+
+  return points + minNotes * (minIndex - 1) + maxNotes * (maxIndex - 1);
 };
 
 module.exports = calculateStylePoints;
