79cbfcd0904c22295f7eada1220090660d825bc9
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-18 23:17:49 +0200

    Changed function calculatestylepoints

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,18 +1,21 @@
-const calculateStylePoints = styleNotes => {
+// const calculateStylePoints = styleNotes => {
   
-  const minNotes = Math.min(...styleNotes);
-  const maxNotes = Math.max(...styleNotes);
+//   const minNotes = Math.min(...styleNotes);
+//   const maxNotes = Math.max(...styleNotes);
 
-  const minIndex = styleNotes.filter(x => x == minNotes).length;
-  const maxIndex = styleNotes.filter(x => x == maxNotes).length;
+//   const minIndex = styleNotes.filter(x => x == minNotes).length;
+//   const maxIndex = styleNotes.filter(x => x == maxNotes).length;
 
-  const total = styleNotes.filter((x) => {
-    return x !== minNotes && x !== maxNotes;
-  });
+//   const total = styleNotes.filter((x) => {
+//     return x !== minNotes && x !== maxNotes;
+//   });
 
-  const points = total.reduce((a, b) => a + b, 0);
+//   const points = total.reduce((a, b) => a + b, 0);
 
-  return points + minNotes * (minIndex - 1) + maxNotes * (maxIndex - 1);
-};
+//   return points + minNotes * (minIndex - 1) + maxNotes * (maxIndex - 1);
+// };
+
+const calculateStylePoints = (styleNotes) => 
+styleNotes.reduce((acc, note) => acc + note, 0) - Math.min(...styleNotes) - Math.max(...styleNotes);
 
 module.exports = calculateStylePoints;
